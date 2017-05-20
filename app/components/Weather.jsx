06332edaa0e9917c64936b1295f678dc94b95987
Godfrey Best <gcb246@stern.nyu.edu>
2017-05-20 12:55:41 -0400

    weather being displayed for the city

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,29 +1,51 @@
 import React, {Component} from 'react';
 import WeatherForm from './WeatherForm';
 import WeatherMessage from './WeatherMessage';
+// var openWeatherMap = require('../api/openWeatherMap');
+import openWeatherMap from '../api/openWeatherMap';
 
 class Weather extends Component {
     constructor (props) {
         super(props);
         this.state = {
-            location: 'Miami',
-            temp: 88
-        }
+            isLoading: false
+        };
         this.handleSearch = this.handleSearch.bind(this);
     }
-    // API Key = 7486057fd080d966249b2b5959530883
     handleSearch (location) {
+        var that = this;
+
         this.setState({
-            location: location,
-            temp: 23
+            isLoading: true
         });
+
+        openWeatherMap.getTemp(location).then((temp) => {
+            that.setState({
+                location: location,
+                temp: temp,
+                isLoading: false
+            });
+        }, (error) => {
+            that.setState({isLoading: false});
+            alert(error);
+        });
+
     }
     render () {
+        var {isLoading, location, temp} = this.state;
+
+        function renderMessage() {
+            if(isLoading) {
+                return <h3>Fetching weather...</h3>;
+            } else if (temp && location) {
+                return <WeatherMessage location={location} temp={temp}/>;
+            }
+        }
         return (
             <div>
                 <h3>Weather Component</h3>
                 <WeatherForm onSearch={this.handleSearch}/>
-                <WeatherMessage location={this.state.location} temp={this.state.temp}/>
+                {renderMessage()}
             </div>
 
 
