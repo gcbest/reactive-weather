f37d252f2afc34445c45792cefbd414a4da8abc9
Godfrey Best <gcb246@stern.nyu.edu>
2017-05-20 14:18:27 -0400

    update about and examples components

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const PORT = process.env.PORT || 3005;
 //Since openWeatherMap uses http instead of https we can use this to redirect it to http
 app.use((req, res, next) => {
     if(req.headers['x-forwarded-proto'] === 'http') {
+        res.redirect('http://' + req.hostname + req.url);
+    } else {
         // run the program as normal
         next();
-    } else {
-        res.redirect('http://' + req.hostname + req.url);
     }
 });
 
