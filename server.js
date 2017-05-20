1b5a13a3d3aa682da9440d1c2cdec644e194ae77
Godfrey Best <gcb246@stern.nyu.edu>
2017-05-20 13:43:46 -0400

    prep for deploy

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var express = require('express');
 
 var app = express();
+const PORT = process.env.PORT || 3005;
+
+//Since openWeatherMap uses http instead of https we can use this to redirect it to http
+app.use((req, res, next) => {
+    if(req.headers['x-forwarded-proto'] === 'http') {
+        // run the program as normal
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
 
 app.use(express.static('public'));
 
-app.listen(3005, function() {
-    console.log('App running on port 3005')
+app.listen(PORT, function() {
+    console.log('App running on port' + PORT);
 });
